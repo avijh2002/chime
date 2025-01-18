e1e074aad5f4b780bc2a6b8737464409462a15d2
Avinash Jha <avijh200221024@gmail.com>
2025-01-19 03:53:15 +0530

    third commit jan 18

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 //state refers to an object that holds information about a component's current situation or condition.
 export const useThemeStore=create((set)=>({
-    theme:localStorage.getItem("chat-theme")||"dark",
+    theme:localStorage.getItem("chat-theme")||"cyberpunk",
     setTheme:(theme)=>{
         localStorage.setItem("chat-theme",theme),
         set({theme})
